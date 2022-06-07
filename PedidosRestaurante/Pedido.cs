3acbd8e8cd824651eecc712932ae7ce007754ee0
Vini1604 <vinicius.oliveira1998@hotmail.com>
2022-06-07 09:42:13 -0300

    Adicionado uma lista de pedidos

--- PedidosRestaurante/Pedido.cs
+++ PedidosRestaurante/Pedido.cs
@@ -6,35 +6,52 @@ namespace PedidosRestaurante
 {
     class Pedido
     {
-        private List<ItemPedido> pedidos { get; set; } = new List<ItemPedido>();
+        private List<ItemPedido> ItensPedido { get; set; } = new List<ItemPedido>();
 
-        public void AdicionarPedido(ItemPedido pedido)
+        public Pedido()
         {
-            pedidos.Add(pedido);
+
+        }
+        public void AdicionarItemPedido(ItemPedido pedido)
+        {
+            ItensPedido.Add(pedido);
+        }
+        public decimal CalculaCustoPedido()
+        {
+            decimal custoPedido = 0;
+            foreach (ItemPedido itemPedido in ItensPedido)
+            {
+                custoPedido += itemPedido.ValorUnitario * itemPedido.Quantidade;
+            }
+            return custoPedido;
         }
 
-        private decimal calculaCustoPedido ()
+        /*
+        private decimal CalculaCustoTotal()
         {
             decimal custoTotal = 0;
-            foreach (ItemPedido pedido in pedidos)
+            foreach (Pedido pedido in Pedidos)
             {
-                custoTotal += pedido.ValorUnitario * pedido.Quantidade;
+                custoTotal += CalculaCustoPedido();
             }
             return custoTotal;
         }
+        */
 
         public override string ToString()
         {
 
-            decimal valorTotal = calculaCustoPedido();
             string notaPedido = "";
+
+            decimal valorPedido = CalculaCustoPedido();
             notaPedido += "*************** Pedido ***************\n";
-            foreach (ItemPedido pedido in pedidos)
+            foreach (ItemPedido itemPedido in ItensPedido)
             {
-                notaPedido += $"{pedido}\n";
+                notaPedido += $"{itemPedido}\n";
             }
-            notaPedido += $"\nValor total do pedido: R${valorTotal:F2}";
+            notaPedido += $"\nValor total do pedido: R${valorPedido:F2}";
             notaPedido += "\n**************************************";
+            notaPedido += "\n";
 
             return notaPedido;
         }
--- PedidosRestaurante/Program.cs
+++ PedidosRestaurante/Program.cs
@@ -1,38 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PedidosRestaurante
 {
     class Program
     {
+        static decimal PrecoTotalPedidos(List<Pedido> pedidos)
+        {
+            decimal precoTotal = 0;
+            foreach (Pedido pedido in pedidos)
+            {
+                precoTotal += pedido.CalculaCustoPedido();
+            }
+            return precoTotal;
+        }
+        
+        
+        
         static void Main(string[] args)
         {
             string descricaoItem;
             int quantidade;
             decimal precoUnitario;
-            char registrarPedido = 'N';
-            Pedido pedido = new Pedido();
-
-            Console.WriteLine("Bem vindo ao Restaurante Dot Net!!!!");
-            Console.WriteLine("Entre com as informacoes dos itens do seu pedido: ");
+            char registrarItemPedido = 'N', registrarPedido = 'N';
+            List<Pedido> pedidos = new List<Pedido>();
+            decimal precoTotal;
 
             do
             {
-                Console.Write("Item: ");
-                descricaoItem = Console.ReadLine();
-                Console.Write("Preco Unitario: R$");
-                precoUnitario = decimal.Parse(Console.ReadLine());
-                Console.Write("Quantidade: ");
-                quantidade = int.Parse(Console.ReadLine());
-                pedido.AdicionarPedido(new ItemPedido(descricaoItem, precoUnitario, quantidade));
+                Console.WriteLine("Bem vindo ao Restaurante Dot Net!!!!");
+                Console.WriteLine("Entre com as informacoes dos itens do seu pedido: ");
+                Pedido pedido = new Pedido();
+                do
+                {
+                    Console.Write("Item: ");
+                    descricaoItem = Console.ReadLine();
+                    Console.Write("Preco Unitario: R$");
+                    precoUnitario = decimal.Parse(Console.ReadLine());
+                    Console.Write("Quantidade: ");
+                    quantidade = int.Parse(Console.ReadLine());
+                    pedido.AdicionarItemPedido(new ItemPedido(descricaoItem, precoUnitario, quantidade));
 
+                    Console.WriteLine();
+                    Console.Write("Deseja adicionar mais itens ao seu pedido (s/n)?");
+                    registrarItemPedido = char.Parse(Console.ReadLine().ToUpper());
+
+                } while (registrarItemPedido == 'S');
+                
+                pedidos.Add(pedido);
                 Console.WriteLine();
-                Console.Write("Deseja adicionar mais itens ao seu pedido (s/n)?");
+                Console.WriteLine("Deseja adicionar mais um pedido (s/n)?");
                 registrarPedido = char.Parse(Console.ReadLine().ToUpper());
+                Console.Clear();
 
             } while (registrarPedido == 'S');
-  
-            Console.WriteLine();
-            Console.WriteLine(pedido);
+
+            foreach (Pedido pedido in pedidos)
+            {
+                Console.WriteLine(pedido);
+            }
+            precoTotal = PrecoTotalPedidos(pedidos);
+            Console.WriteLine($"Valor total: R${precoTotal:F2}");
         }
     }
 }
